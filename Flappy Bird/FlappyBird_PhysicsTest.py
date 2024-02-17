2046628c43ba22340cab46bb17cc8c0ad6342fa0
Noah Honold <noah.honold@gmx.de>
2024-02-17 19:16:27 +0100

    made variable changes

--- Flappy Bird/FlappyBird_PhysicsTest.py
+++ Flappy Bird/FlappyBird_PhysicsTest.py
@@ -4,6 +4,7 @@ pygame.init()
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 640
+BIRD_SIZE = 40
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Flappy Bird")
@@ -24,7 +25,7 @@ while run:
     player_pos[1] += player_vel
     
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), (player_pos[0], player_pos[1], 40, 40))
+    pygame.draw.rect(screen, (255, 0, 0), (player_pos[0], player_pos[1], BIRD_SIZE, BIRD_SIZE))
     
     key = pygame.key.get_pressed()
     if key[pygame.K_SPACE]:
@@ -33,8 +34,8 @@ while run:
     if player_pos[1] < 0:
         player_pos[1] = 0
         
-    if player_pos[1] > SCREEN_HEIGHT - 40:
-        player_pos[1] = SCREEN_HEIGHT - 40
+    if player_pos[1] > SCREEN_HEIGHT - BIRD_SIZE:
+        player_pos[1] = SCREEN_HEIGHT - BIRD_SIZE
         player_vel = 0
     
     for event in pygame.event.get():
