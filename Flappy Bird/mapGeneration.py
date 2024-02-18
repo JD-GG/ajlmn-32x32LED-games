4d9afa95527df0a4c8138e521b9539a06acf55b7
JD-GG <104784838+JD-GG@users.noreply.github.com>
2024-02-18 10:34:06 +0100

    Add comments to map generation

--- Flappy Bird/mapGeneration.py
+++ Flappy Bird/mapGeneration.py
@@ -4,16 +4,16 @@ import colors as c
 import settings as s
 
 def init_pillar_pos_x():
-    first_pillar_position = s.SCREEN_WIDTH + s.PILLAR_WIDTH
-    start_pos_x = [None] * s.PILLAR_COUNT
+    first_pillar_position = s.SCREEN_WIDTH + s.PILLAR_WIDTH# First pillar is just outside of screen
+    start_pos_x = [None] * s.PILLAR_COUNT# Init position_x array
     for i in range(s.PILLAR_COUNT):
-        start_pos_x[i] = first_pillar_position + (i * s.PILLAR_WIDTH) + (i * s.PILLAR_GAP_WIDTH)
+        start_pos_x[i] = first_pillar_position + (i * s.PILLAR_WIDTH) + (i * s.PILLAR_GAP_WIDTH)# Place all pillars side by side
     return start_pos_x
 
 def init_pillar_pos_y():
-    random_pos_y = [None] * s.PILLAR_COUNT
+    random_pos_y = [None] * s.PILLAR_COUNT# Init position_y array
     for i in range(s.PILLAR_COUNT):
-        random_pos_y[i] = get_random_pos_y()
+        random_pos_y[i] = get_random_pos_y()# Generate random y values for each
     return random_pos_y
 
 def get_random_pos_y():
