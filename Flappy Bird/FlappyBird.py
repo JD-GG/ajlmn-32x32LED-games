e3b823ec6f536b225ca3c63db0d25bc9f126260c
JD-GG <104784838+JD-GG@users.noreply.github.com>
2024-02-19 13:03:42 +0100

    Matrix implementation
    should work hopefully

--- Flappy Bird/FlappyBird.py
+++ Flappy Bird/FlappyBird.py
@@ -1,8 +1,12 @@
+import sys
 import pygame
-from mapGeneration import init_pillar_pos_x, init_pillar_pos_y, get_random_pos_y, draw_pillars
-from output import draw_matrix_representation, draw_matrix_grid, draw_position_markers
-import colors as c
+from mapGeneration import init_pillar_pos_x, init_pillar_pos_y, get_random_pos_y
+from output import draw_screen, draw_matrix, draw_matrix_representation, draw_matrix_grid, draw_position_markers
 import settings as s
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
+
+# Matrix
+matrix = rgbmatrix.RGBMatrix(32, 1, 1)# 32 Pixels, 1 Pannel, 1 => Standard HAT
 
 # Init
 pygame.init()
@@ -76,6 +80,7 @@ while run:
 
     # Drawing
     draw_screen(screen, player_pos_x, player_pos_y, pillar_pos_x, pillar_pos_y)
+    draw_matrix(screen, matrix)
     draw_matrix_representation(screen)
     draw_matrix_grid(screen)
     draw_position_markers(screen, player_pos_x, player_pos_y, pillar_pos_x, pillar_pos_y)# Drawing markers after matrix conversion so they won't show up in the image
--- Flappy Bird/output.py
+++ Flappy Bird/output.py
@@ -1,7 +1,7 @@
 import pygame
 import colors as c
 import settings as s
-#from rgbmatrix import RGBMatrix, RGBMatrixOptions
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 def draw_screen(screen, player_pos_x, player_pos_y, pillar_pos_x, pillar_pos_y):
     screen.fill(c.SKY_BLUE)
@@ -9,7 +9,14 @@ def draw_screen(screen, player_pos_x, player_pos_y, pillar_pos_x, pillar_pos_y):
     pygame.draw.rect(screen, c.FLAPPY_ORANGE, (player_pos_x, player_pos_y, s.PLAYER_WIDTH, s.PLAYER_WIDTH))# Player
     pygame.draw.rect(screen, c.GROUND_BROWN, (0, s.SCREEN_HEIGHT - s.GROUND_HEIGHT, s.SCREEN_WIDTH, s.GROUND_HEIGHT))# Ground dirt
     pygame.draw.rect(screen, c.LIGHT_GREEN, (0, s.SCREEN_HEIGHT - s.GROUND_HEIGHT, s.SCREEN_WIDTH, s.PIXEL_WIDTH))# Ground top layer
-   
+
+def draw_matrix(screen, matrix):
+    for y in range(32):
+        for x in range(32):
+            color_rgba = screen.get_at((x, y))# get color in format (r, g, b, t)
+            color_rgb = color_rgba[:3]# Cut the alpha channel of
+            matrix.SetPixel(x, y, color_rgb)
+
 def draw_pillars(screen, pillar_pos_x, pillar_pos_y):
     for i in range(s.PILLAR_COUNT):
         pygame.draw.rect(screen, c.LIGHT_GREEN, (pillar_pos_x[i], 0, s.PIXEL_WIDTH, s.SCREEN_HEIGHT))# Pillar left
@@ -26,7 +33,6 @@ def draw_matrix_representation(screen):
             pos_y = y * s.PIXEL_WIDTH
             color = screen.get_at((pos_x, pos_y))# get color in format (r, g, b, t)
             # set pixel or something idk
-            
             pygame.draw.rect(screen, color, (pos_x + s.SCREEN_WIDTH, pos_y, s.PIXEL_WIDTH, s.PIXEL_WIDTH))# Single Pixel representation
 
     pygame.draw.rect(screen, (0, 0, 0), (s.SCREEN_WIDTH, 0, 1, s.SCREEN_HEIGHT))# Seperating line
