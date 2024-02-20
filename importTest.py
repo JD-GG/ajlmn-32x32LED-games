cccbb703308d553bcb8930be89f112b544fb617e
JD-GG <104784838+JD-GG@users.noreply.github.com>
2024-02-20 12:58:29 +0100

    Update importTest.py

--- importTest.py
+++ importTest.py
@@ -10,10 +10,12 @@ options.hardware_mapping = 'adafruit-hat'# Important don't forget
 
 matrix = RGBMatrix(options = options)
 
+offset_canvas = matrix.CreateFrameCanvas()
+
 for y in range(32):
     for x in range(32):
-        matrix.SetPixel(x, y, 255, 0, 0)
-        matrix.Update()
+        offset_canvas.SetPixel(x, y, 255, 0, 0)
+        offset_canvas = matrix.SwapOnVSync(offset_canvas)
 
 # Run with sudo ./importTest.py after chmod +x importTest.py
 # chmod should not modify the file in any way tho
