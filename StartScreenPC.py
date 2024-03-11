0416f06acd5839317bbd2a5e6f82db740d8568e7
JD-GG <104784838+JD-GG@users.noreply.github.com>
2024-03-11 18:50:54 +0100

    better game select

--- StartScreenPC.py
+++ StartScreenPC.py
@@ -2,7 +2,10 @@ import os
 import pygame
 from pygame.locals import *
 import FlappyBird.settings as s
+import FlappyBird.colors as fc
+from FlappyBird.output import draw_matrix_representation
 from FlappyBird.FlappyBird import flappy_bird_game
+import GeometryDash.Variables as v
 from GeometryDash.Main import geometry_dash_game
 from Snake.snake import snake_game
 
@@ -26,9 +29,59 @@ for joystick in joysticks:
 screen = pygame.display.set_mode((s.SCREEN_WIDTH*2, s.SCREEN_HEIGHT))
 pygame.display.set_caption("Startscreen")
 
-# Logic to select game can go right here
-flappy_bird_game(screen)
-geometry_dash_game(screen)
-snake_game(screen)
+SCREEN_HALF = s.SCREEN_WIDTH // 2
+select_box_x = 0
+select_box_y = 0
+
+run = True
+while(run):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        elif event.type == pygame.KEYDOWN and event.key == K_LEFT:
+            select_box_x = 0
+        elif event.type == pygame.KEYDOWN and event.key == K_RIGHT:
+            select_box_x = SCREEN_HALF
+        elif event.type == pygame.KEYDOWN and event.key == K_UP:
+            select_box_y = 0
+        elif event.type == pygame.KEYDOWN and event.key == K_DOWN:
+            select_box_y = SCREEN_HALF
+        # Stop game and go to game select
+        elif event.type == pygame.KEYDOWN and event.key == K_RETURN:
+            if(select_box_x == 0 and select_box_y == 0):
+                flappy_bird_game(screen)
+            elif(select_box_x == 0 and select_box_y == SCREEN_HALF):
+                snake_game(screen)
+            elif(select_box_x == SCREEN_HALF and select_box_y == 0):
+                geometry_dash_game(screen)
+            elif(select_box_x == SCREEN_HALF and select_box_y == SCREEN_HALF):
+                run = False
+
+    # Flappy Bird Mockup
+    pygame.draw.rect(screen, fc.DARK_SKY_BLUE, (0, 0, SCREEN_HALF, SCREEN_HALF))
+    pygame.draw.rect(screen, fc.FLAPPY_ORANGE, (60, 60, s.PLAYER_WIDTH, s.PLAYER_WIDTH))# Player
+    pygame.draw.rect(screen, fc.LIGHT_GREEN, (200, 0, s.PIXEL_WIDTH, SCREEN_HALF))# Pillar left
+    pygame.draw.rect(screen, fc.GREEN, (200 + s.PIXEL_WIDTH, 0, s.PIXEL_WIDTH, SCREEN_HALF))# Pillar center
+    pygame.draw.rect(screen, fc.DARK_GREEN, (200 + (s.PIXEL_WIDTH * 2), 0, s.PIXEL_WIDTH, SCREEN_HALF))# Pillar right
+    pygame.draw.rect(screen, fc.DARK_SKY_BLUE, (200, 80, s.PILLAR_WIDTH, 80))# Pillar vertical gap
+    pygame.draw.rect(screen, fc.GROUND_BROWN, (0, SCREEN_HALF - s.GROUND_HEIGHT, SCREEN_HALF, s.GROUND_HEIGHT))# Ground dirt
+    pygame.draw.rect(screen, fc.LIGHT_GREEN, (0, SCREEN_HALF - s.GROUND_HEIGHT, SCREEN_HALF, s.PIXEL_WIDTH))#Grass
+
+    # Geometry Dash Mockup
+    pygame.draw.rect(screen, fc.BLACK, (SCREEN_HALF, 0, SCREEN_HALF, SCREEN_HALF))
+    pygame.draw.rect(screen, v.GREEN, (SCREEN_HALF, SCREEN_HALF - v.GROUND, SCREEN_HALF, v.GROUND))
+    pygame.draw.rect(screen, v.RED, (SCREEN_HALF + 60, 180, s.PLAYER_WIDTH, s.PLAYER_WIDTH))
+
+    # Snake Mockup
+    pygame.draw.rect(screen, fc.BLACK, (0, SCREEN_HALF, SCREEN_HALF, SCREEN_HALF))
+
+    # Off Switch
+    pygame.draw.rect(screen, fc.BLACK, (SCREEN_HALF, SCREEN_HALF, SCREEN_HALF, SCREEN_HALF))
+
+    # Select Box
+    pygame.draw.rect(screen, (255, 255, 255), (select_box_x, select_box_y, SCREEN_HALF, SCREEN_HALF), s.PIXEL_WIDTH)
+    
+    draw_matrix_representation(screen)
+    pygame.display.update()
 
 pygame.quit()
