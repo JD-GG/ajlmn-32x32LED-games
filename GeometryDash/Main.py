efadb534e82c8b255b65aff4faf59fde937f3e46
JD-GG <104784838+JD-GG@users.noreply.github.com>
2024-03-15 14:17:23 +0100

    change music import

--- GeometryDash/Main.py
+++ GeometryDash/Main.py
@@ -1,4 +1,8 @@
+#pcm.!default {type hw card 1}
+#ctl.!default {type hwcard 1}
+
 import pygame
+import GeometryDash.soundplayer as sound
 import GeometryDash.Variables as v
 import GeometryDash.collision as collision
 import GeometryDash.drawing as drawing
@@ -14,6 +18,11 @@ except ImportError:
 
 def geometry_dash_game(screen, matrix, offset_canvas):
     clock = pygame.time.Clock()
+
+    # Song, still to be done
+    song = sound.SoundPlayer("./GeometryDash/music.mp3", 1)
+    song.play()
+
     run = True
     while run:
         #time.sleep(0.03) # 30FPS
@@ -32,3 +41,4 @@ def geometry_dash_game(screen, matrix, offset_canvas):
         else:
             draw_matrix_representation(screen)
             pygame.display.update()
+    song.stop()
