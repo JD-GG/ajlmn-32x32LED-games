efb0994a1edaf6fa2b30a45c1feeab78196abd37
JD-GG <104784838+JD-GG@users.noreply.github.com>
2024-03-18 23:10:42 +0100

    death wav

--- GeometryDash/Main.py
+++ GeometryDash/Main.py
@@ -19,12 +19,13 @@ def geometry_dash_game(screen, matrix, offset_canvas):
     clock = pygame.time.Clock()
 
     audio_file_path = "./GeometryDash/music.wav"
-    player = None
+    death_file_path = "./GeometryDash/death.wav"
+    music_player = None
+    death_player = None
+    first_death_loop = False
     if started_on_pi:
-        player = sound.AudioPlayer(audio_file_path)
-    else:
-        pygame.mixer.music.load(audio_file_path)
-        pygame.mixer.music.play(0, 1.0)
+        music_player = sound.AudioPlayer(audio_file_path)
+        death_player = sound.AudioPlayer(death_file_path)
 
     run = True
     running = True
@@ -33,12 +34,16 @@ def geometry_dash_game(screen, matrix, offset_canvas):
 
         if run:
             if started_on_pi:
-                player.play()
+                death_player.stop()
+                music_player.play()
             else:
-                pygame.mixer.music.rewind()
+                pygame.mixer.music.stop()
+                pygame.mixer.music.load(audio_file_path)
                 pygame.mixer.music.play(0, 1.0)
 
         while run and running:
+            first_death_loop = True
+
             tickTime = clock.tick(60) / 1000
             screen.fill((0, 0, 0))
             collision.PlayerOnGround()
@@ -57,10 +62,14 @@ def geometry_dash_game(screen, matrix, offset_canvas):
             v.player_vel += v.gravity * tickTime  
             v.player_pos += v.player_vel
 
-        if started_on_pi:
-            player.stop()
-        else:
+        if started_on_pi and first_death_loop:
+            music_player.stop()
+            death_player.play()
+        elif first_death_loop:
             pygame.mixer.music.stop()
+            pygame.mixer.music.load(death_file_path)
+            pygame.mixer.music.play(0, 1.0)
+        first_death_loop = False
 
         run = userInput.checkAnyInput()
         running = userInput.checkInput()
