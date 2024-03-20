a466d6f4fc2c307a4d0be8ed193dafefeb0a2528
JD-GG <104784838+JD-GG@users.noreply.github.com>
2024-03-20 15:01:37 +0100

    Make colision work in all cases

--- GeometryDash/collision.py
+++ GeometryDash/collision.py
@@ -10,12 +10,20 @@ def PlayerOnGround():
 def PlayerHitObstical(Rect):
     player_hitbox = pygame.Rect(v.PLAYERX, v.player_pos, v.PLAYERSIZE, v.PLAYERSIZE)
     if player_hitbox.colliderect(Rect):
-
-        if v.player_vel > 0 and player_hitbox.right > Rect.left and player_hitbox.left < Rect.left and player_hitbox.bottom-8 > Rect.top: #Dead falling down
+        #Dead coming into block from left falling down
+        if v.player_vel > 0 and player_hitbox.right > Rect.left and player_hitbox.left < Rect.left and player_hitbox.bottom-8 > Rect.top: 
             return True
-        elif v.player_vel <= 0 and player_hitbox.right > Rect.left and player_hitbox.left < Rect.left: #Dead jumping up
+        
+        #Dead coming into block from left jumping up
+        elif v.player_vel <= 0 and player_hitbox.right > Rect.left and player_hitbox.left < Rect.left:
             return True
-        elif player_hitbox.bottom > Rect.top and player_hitbox.top < Rect.top: #On Top
+        
+        # Dead jumpong up into bock
+        elif player_hitbox.top < Rect.bottom and player_hitbox.bottom > Rect.bottom:
+            return True
+        
+        #On Top
+        elif player_hitbox.bottom > Rect.top and player_hitbox.top < Rect.top:
             v.player_pos = Rect.top - v.PLAYERSIZE
             v.player_vel = 0
             v.on_ground = True
